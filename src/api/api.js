5bfca8e7bfd37036a2425465ba5be8ce6e58382f
dongrunshi <dongrunshi_u@163.com>
2019-06-06 09:01:41 +0800

    Half completed front-end

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 let base = '';
 
 //url都要改!!!!!!我随便写的
+//有些还需要改成axios.post
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };//登陆
 
@@ -36,4 +37,33 @@ export const OvertimeRequest = params => {return axios.get('/overtime/user',{par
 
 export const getHours = params => { return axios.get('/hours', { params:params });}//获取工时记录
 
-export const getShit = params => {return axios.get('/shift',{params:params});}
+export const getDepartmentList = params => {return axios.get('/department',{params:params});}//获取部门列表
+
+export const addDepartment = params => { return axios.get('/department',{ params:params });}//添加部门
+
+export const editDepartment = params => { return axios.get('/department',{ params:params});}//编辑部门
+
+export const getDepartment = params => { return axios.get('/department',{params: params });}//获取指定部门信息
+
+export const deletDepartmeny = params => { return axios.get('/department',{ params:params })}//删除部门
+
+export const getShiftList = params => {return axios.get('/shift',{ params:params });}//获取排班记录
+
+export const addShift = params => { return axios.get('/shift/user',{ params:params});}//添加排班
+
+export const getDepartmentShift = params => { return axios.get('/shift/department',{ params: params});}//获取部门排班
+
+export const getFaceUser = params => { return axios.get('/face/user',{ params: params});}//获取指定用户可用的人脸信息
+
+export const editFaceUser = params => { return axios.get('/face/user',{ params: params});}//更新指定用户人脸信息
+
+export const faceApproval = params => { return axios.get('face', { params: params });}//审核人脸信息
+
+export const getSignStatus = params => { return axios.get('/sign/user',{ params: params });}//获取当前用户签到状态
+
+export const getSignQrcode = params => { return axios.get('/sign/qrcode',{params: params});}//获取二维码
+
+export const signQrcode = params => { return axios.get('/sign/qrcode/user',{ params: params});}//二维码签到
+
+export const signFace = params =>{ return axios.get('/sign/face/user',{params: params});}//人脸签到
+
---
+++ src/components/department/department.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "department"
+    }
+</script>
+
+<style scoped>
+
+</style>
