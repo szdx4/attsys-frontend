7ec966707f458f9fb41f97d1df06807dd38c61bc
dongz666@outlook.com <dongzhiwei2021@outlook.com>
2019-06-07 12:42:03 +0800

    update api.js a lot

--- src/api/api.js
+++ src/api/api.js
@@ -32,89 +32,89 @@ export const addUser = params => { return axios.post(`${base}/user/add`, { param
 export const getLeaveListPage = params => {return axios.get( `${base}/leave`, { params: params });};
 
 //获取指定用户请假信息
-export const getLeaveUser = params => {return axios.get(`${base}/leave/user`,{params:params})};
+export const getLeaveUser = (id, params) => {return axios.get(`${base}/leave/user/${id}`,{params:params})};
 
 //申请请假
-export const LeaveRequest = params =>{ return axios.post( `${base}/leave/user`, { params: params });};
+export const LeaveRequest = (id, params) =>{ return axios.post( `${base}/leave/user/${id}`, { params: params });};
 
 //审核请假申请
-export const LeaveRequestApproval = params => { return axios.post(`${base}/leave/user`, { params: params }); };
+export const LeaveRequestApproval = (id, params) => { return axios.put(`${base}/leave/user/${id}`, { params: params }); };
 
 
 //**************************************************加班管理**************************************************
 
 //获取加班列表
-export const getOvertimeList = params => {return axios.get('/overtime',{params: params});};
-
-//审核加班申请
-export const OvertimeApproval = params => { return axios.post('/overtime', {params: params});};
+export const getOvertimeList = params => {return axios.get(`${base}/overtime`,{params: params});};
 
 //获取指定用户加班申请
-export const getOvertimeUser = params => { return axios.get('/overtime',{params:params});};
+export const getOvertimeUser = (id, params)  => { return axios.get(`${base}/overtime/user/${id}`,{params:params});};
 
 //申请加班
-export const OvertimeRequest = params => {return axios.post('/overtime/user',{params:params});};
+export const OvertimeRequest = (id, params)  => { return axios.post(`${base}/overtime/user/${id}`,{params:params});};
+
+//审核加班申请
+export const OvertimeApproval = (id, params) => { return axios.put(`${base}/overtime/user/${id}`, {params: params});};
 
 
 //**************************************************工时列表**************************************************
 
 //获取工时记录
-export const getHours = params => { return axios.get('/hours', { params:params });};
+export const getHours = params => { return axios.get(`${base}/hours`, { params:params });};
 
 
 //**************************************************部门管理**************************************************
 
 //获取部门列表
-export const getDepartmentList = params => {return axios.get('/department',{params:params});};
+export const getDepartmentList = params => {return axios.get(`${base}/department`,{params:params});};
 
 //添加部门
-export const addDepartment = params => { return axios.post('/department',{ params:params });};
+export const addDepartment = params => { return axios.post(`${base}/department`,{ params:params });};
 
 //编辑部门
-export const editDepartment = params => { return axios.post('/department',{ params:params});};
+export const editDepartment = (id, params) => { return axios.put(`${base}/department/${id}`,{ params:params});};
 
 //获取指定部门信息
-export const getDepartment = params => { return axios.get('/department',{params: params });};
+export const getDepartment = (id, params) => { return axios.get(`${base}/department/${id}`,{params: params });};
 
 //删除部门
-export const deletDepartment = params => { return axios.post('/department',{ params:params })};
+export const deletDepartment = (id, params) => { return axios.delete(`${base}/department/${id}`,{ params:params })};
 
 
-//**************************************************排版管理**************************************************
+//**************************************************排班管理**************************************************
 
-//获取排班记录
-export const getShiftList = params => {return axios.get('/shift',{ params:params });};
+//获取排班列表
+export const getShiftList = params => {return axios.get(`${base}/shift`,{ params:params });};
 
 //添加排班
-export const addShift = params => { return axios.post('/shift/user',{ params:params});};
+export const addShift = (id, params) => { return axios.post(`${base}/shift/user/${id}`,{ params:params});};
 
 //获取部门排班
-export const getDepartmentShift = params => { return axios.get('/shift/department',{ params: params});};
+export const getDepartmentShift = params => { return axios.get(`${base}/shift/department`,{ params: params});};
 
 
 //**************************************************人脸信息**************************************************
 
 //获取指定用户可用的人脸信息
-export const getFaceUser = params => { return axios.get('/face/user',{ params: params});};
+export const getFaceUser = (id, params) => { return axios.get(`${base}/face/user/${id}`,{ params: params});};
 
 //更新指定用户人脸信息
-export const editFaceUser = params => { return axios.post('/face/user',{ params: params});};
+export const editFaceUser = (id, params) => { return axios.post(`${base}/face/user/${id}`,{ params: params});};
 
 //审核人脸信息
-export const faceApproval = params => { return axios.post('face', { params: params });};
+export const faceApproval = (id, params) => { return axios.put(`${base}/face/user/${id}`, { params: params });};
 
 
 //**************************************************签到管理**************************************************
 
 //获取当前用户签到状态
-export const getSignStatus = params => { return axios.get('/sign/user',{ params: params });};
+export const getSignStatus = (id, params) => { return axios.get(`${base}/sign/user/${id}`,{ params: params });};
 
 //获取二维码
-export const getSignQrcode = params => { return axios.get('/sign/qrcode',{params: params});};
+export const getSignQrcode = params => { return axios.get(`${base}/sign/qrcode`,{params: params});};
 
 //二维码签到
-export const signQrcode = params => { return axios.post('/sign/qrcode/user',{ params: params});};
+export const signQrcode = (id, params) => { return axios.post(`${base}/sign/qrcode/user/${id}`,{ params: params});};
 
 //人脸签到
-export const signFace = params =>{ return axios.post('/sign/face/user',{params: params});};
+export const signFace = (id, params) =>{ return axios.post(`${base}/sign/face/user/${id}`,{params: params});};
 
