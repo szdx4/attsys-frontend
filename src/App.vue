59aae9e8692c7dd008e48c493d1c2e88912ae2b0
kotoyuuko <kokoro@core.moe>
2019-06-11 20:15:47 +0800

    修复登录用户菜单需要刷新显示的问题

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,95 @@
 <template>
-    <div id="app">
-        <transition name="fade"
-                    mode="out-in">
-            <router-view></router-view>
-        </transition>
-    </div>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'app',
-        components: {},
-        methods: {
+export default {
+  name: "app",
+  components: {},
+  methods: {
+    // logout for other page
+    logout: function() {
+      sessionStorage.removeItem("user");
+      localStorage.removeItem("token");
+      localStorage.removeItem("id");
+      localStorage.removeItem("role");
+      localStorage.removeItem("expired_at");
+      this.$router.push("/login");
+    },
 
-            // logout for other page
-            logout: function () {
-                sessionStorage.removeItem('user');
-                localStorage.removeItem('token');
-                localStorage.removeItem('id');
-                localStorage.removeItem('role');
-                localStorage.removeItem('expired_at');
-                this.$router.push('/login');
-            },
-
-            verify() {
-                let token = localStorage.getItem('token');
-                if (!token) {
-                    this.$router.push({path: '/login'});
-                } else {
-                    let expired_at = localStorage.getItem('expired_at');
-                    let expired_date = new Date(expired_at);
-                    let now_date = new Date();
-                    if (now_date > expired_date) {
-                        this.$router.push({path: '/login'});
-                        this.logout();
-                    }
-
-                }
-
-            },
-
-        },
-        mounted() {
-            this.verify();
+    verify() {
+      let token = localStorage.getItem("token");
+      if (!token) {
+        this.$router.push({ path: "/login" });
+      } else {
+        let expired_at = localStorage.getItem("expired_at");
+        let expired_date = new Date(expired_at);
+        let now_date = new Date();
+        if (now_date > expired_date) {
+          this.$router.push({ path: "/login" });
+          this.logout();
         }
+      }
     }
-
+  },
+  mounted() {
+    this.verify();
+  }
+};
 </script>
 
 <style lang="scss">
-    body {
-        margin: 0px;
-        padding: 0px;
-        /*background: url(assets/bg1.jpg) center !important;
+body {
+  margin: 0px;
+  padding: 0px;
+  /*background: url(assets/bg1.jpg) center !important;
             background-size: cover;*/
-        // background: #1F2D3D;
-        font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-        font-size: 14px;
-        -webkit-font-smoothing: antialiased;
-    }
+  // background: #1F2D3D;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
+}
 
-    #app {
-        position: absolute;
-        top: 0px;
-        bottom: 0px;
-        width: 100%;
-    }
+#app {
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  width: 100%;
+}
 
-    .el-submenu [class^=fa] {
-        vertical-align: baseline;
-        margin-right: 10px;
-    }
+.el-submenu [class^="fa"] {
+  vertical-align: baseline;
+  margin-right: 10px;
+}
 
-    .el-menu-item [class^=fa] {
-        vertical-align: baseline;
-        margin-right: 10px;
-    }
+.el-menu-item [class^="fa"] {
+  vertical-align: baseline;
+  margin-right: 10px;
+}
 
-    .toolbar {
-        background: #f2f2f2;
-        padding: 10px;
-        //border:1px solid #dfe6ec;
-        margin: 10px 0px;
+.toolbar {
+  background: #f2f2f2;
+  padding: 10px;
+  //border:1px solid #dfe6ec;
+  margin: 10px 0px;
 
-        .el-form-item {
-            margin-bottom: 10px;
-        }
-    }
+  .el-form-item {
+    margin-bottom: 10px;
+  }
+}
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: all .2s ease;
-    }
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
 
-    .fade-enter,
-    .fade-leave-active {
-        opacity: 0;
-    }
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style>
